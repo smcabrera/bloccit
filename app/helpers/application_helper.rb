e747ec1175cfc75a1ccb087494976f207aa11b50
Stephen Mariano Cabrera <stephen.m.cabrera@gmail.com>
2014-07-04 13:05:59 -0500

    Installed bootstrap and styled nav area

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def created_by
+  'Created as part of the bloc apprenticeship by me: <a href="smcabrera.github.io">Stephen Mariano</a> with the help of my mentor <a href="http://about.me/matthieu.tanguay.carel">Matt</a>'
+  end
+
+  def my_name
+    "Stephen Cabrera"
+  end
 end
