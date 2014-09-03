488d6aed09a6ba9fb358031132e1e587b7ca4cc3
Stephen Mariano Cabrera <stephen.m.cabrera@gmail.com>
2014-09-03 09:25:00 -0500

    Refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Bloccit::Application.routes.draw do
   devise_for :users
+  resources :users, only: [:update]
 
   resources :topics do
-    resources :posts, except: [:index] 
+    resources :posts, except: [:index]
   end
 
   get 'about' => 'welcome#about'
