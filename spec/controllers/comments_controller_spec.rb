ed7da6b3037c9f9832fc376ec29c2f6d8d8385fb
Stephen Mariano Cabrera <stephen.m.cabrera@gmail.com>
2014-09-16 16:45:13 -0500

    Create spec for comments controller

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -2,4 +2,29 @@ require 'rails_helper'
 
 RSpec.describe CommentsController, :type => :controller do
 
+
+
+  describe "#destroy" do
+
+
+
+    it "destroy the appropriate comment" do
+      #Setup
+      Comment.create
+      #Execution Phase
+      delete :destroy
+
+      #
+      #Validaiton Phase
+      Comment.count.should == 0
+      #
+      #
+      #Cleanup Phase
+
+    end
+  end
 end
+
+
+
+
