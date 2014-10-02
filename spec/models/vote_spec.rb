17c11b8c7f76aeedb3fd369d9c5493d0aa963d7c
Stephen Mariano Cabrera <stephen.m.cabrera@gmail.com>
2014-10-02 04:42:11 -0500

    Create tests for a voting feature

---
+++ spec/models/vote_spec.rb
@@ -0,0 +1,12 @@
+describe Vote do
+  describe "validations" do
+    describe "value validation" do
+      it "only allows -1 or 1 as values" do
+        v = Vote.new(value: 1)
+        expect( v.valid? ).to eq(true)
+        v = Vote.new(value: 2)
+        expect( v.valid? ).to eq(false)
+      end
+    end
+  end
+end
