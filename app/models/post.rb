dc3bc0cbcf79bb7d06eec31423b8990d3bb1cc10
Stephen Mariano Cabrera <stephen.m.cabrera@gmail.com>
2015-02-09 18:52:17 -0500

    Started on voting functionality

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,9 +10,21 @@ class Post < ActiveRecord::Base
 
   validates :title, length: {minimum: 5 }, presence: true
   validates :body, length: {minimum: 20 }, presence: true
-  validates :topic, presence: true
-  validates :user, presence: true
+  #validates :topic, presence: true
+  #validates :user, presence: true
 
   mount_uploader :image, ImageUploader
+
+  def up_votes
+    votes.where(value: 1).count
+  end
+
+  def down_votes
+    votes.where(value: -1).count
+  end
+
+  def points
+    votes.sum("value")
+  end
 end
 
